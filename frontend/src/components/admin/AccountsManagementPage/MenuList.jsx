332832558b82abfb6d860b5e6af4410deb40b0ae
nam-dao <122555780+nam-dao@users.noreply.github.com>
2025-03-26 19:28:42 +0700

    modify project directory structure

---
+++ frontend/src/components/admin/AccountsManagementPage/MenuList.jsx
@@ -0,0 +1,68 @@
+import { Menu } from "antd";
+import { useNavigate, useLocation } from "react-router";
+import {
+  HomeOutlined,
+  CalendarOutlined,
+  FileOutlined,
+  LogoutOutlined,
+  VideoCameraOutlined,
+  TeamOutlined,
+  FolderOpenOutlined,
+} from "@ant-design/icons";
+
+const menuItems = [
+  { key: "dashboard", label: "Dashboard", icon: <HomeOutlined /> },
+  { key: "calendar", label: "Calendar", icon: <CalendarOutlined /> },
+  {
+    key: "accounts-management",
+    label: "Accounts management",
+    icon: <TeamOutlined />,
+  },
+  { key: "blog-management", label: "Blog management", icon: <FileOutlined /> },
+  {
+    key: "document-management",
+    label: "Document management",
+    icon: <FolderOpenOutlined />,
+  },
+  {
+    key: "meeting-management",
+    label: "Meeting management",
+    icon: <VideoCameraOutlined />,
+  },
+];
+
+const bottomItems = [
+  { key: "logout", label: "Log out", icon: <LogoutOutlined /> },
+];
+
+export default function MenuList() {
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  const currentKey = location.pathname.split("/")[2] || "dashboard";
+
+  return (
+      <div style={{ display: 'flex', flexDirection: 'column', height: 'calc(100vh - 6rem)' }}>
+        {/* Menu chính */}
+        <Menu
+            theme="dark"
+            mode="inline"
+
+            selectedKeys={[currentKey]}
+            items={menuItems}
+            onClick={(e) => navigate(`/admin/${e.key}`)}
+        />
+
+        {/* Menu dưới cùng */}
+        <div style={{ marginTop: "auto" }}>
+          <Menu
+              theme="dark"
+              mode="inline"
+              selectedKeys={[currentKey]}
+              items={bottomItems}
+              onClick={(e) => navigate(`/admin/${e.key}`)}
+          />
+        </div>
+      </div>
+  );
+}
