ae59d20e43c23bc7d5fa1fceabb6585e358aa706
nam-dao <122555780+nam-dao@users.noreply.github.com>
2025-03-28 09:51:30 +0700

    Edit the Blog Page

--- frontend/src/components/student/BlogPage/ModalBlogActions.jsx
+++ frontend/src/components/student/BlogPage/ModalBlogActions.jsx
@@ -1,14 +1,29 @@
 import { CloseOutlined, DiffOutlined } from "@ant-design/icons";
 import { Button, Form, Input, Modal, Space, notification } from "antd";
-import { createBlog } from "../../../../api_service/blog_service";
+import { createBlog, editBlog } from "../../../../api_service/blog_service";
 const userId = localStorage.getItem("userId");
 const userRole = localStorage.getItem("role") || "Student";
-export default function ModalBlogActions({ isModalOpen, onOK, onCancel }) {
+export default function ModalBlogActions({
+  isModalOpen,
+  onOK,
+  onCancel,
+  blogProp = null,
+}) {
+  const initialValues = blogProp
+    ? {
+        title: blogProp.title,
+        content: blogProp.content,
+        uploaderId: blogProp.userId,
+        uploaderType: blogProp.userRole,
+        tags: blogProp.tags.join(", "),
+        imageUrl: blogProp.imageUrl,
+      }
+    : {};
   const [form] = Form.useForm();
+  console.log("blogProp", blogProp);
   const handleSubmitForm = async () => {
     try {
       const values = await form.validateFields();
-      console.log(values);
       const blog = {
         title: values.title,
         content: values.content,
@@ -19,9 +34,22 @@ export default function ModalBlogActions({ isModalOpen, onOK, onCancel }) {
           : [],
         imageUrl: values.imageUrl,
       };
-      await createBlog(blog);
+      if (blogProp) {
+        // nếu nó có prop này tức là chỉnh sửa thì sẽ updateBlog
+        const updatedBlog = {
+          title: blog?.title,
+          content: blog?.content,
+          tags: blog?.tags,
+          imageUrl: blog?.imageUrl,
+        };
+        await editBlog(blogProp?._id, updatedBlog);
+      } else {
+        await createBlog(blog);
+      }
       notification.success({
-        message: `Post ${blog.title} Blog successful`,
+        message: `${blogProp ? "Update" : "Post"} ${
+          blog.title
+        } Blog successful`,
         duration: 3, // Thời gian tự động đóng (giây)
       });
       onOK();
@@ -34,12 +62,13 @@ export default function ModalBlogActions({ isModalOpen, onOK, onCancel }) {
     form.resetFields();
     onCancel();
   };
+  console.log("blogProp", blogProp);
   return (
     <Modal
       title={
         <Space size="small">
           <DiffOutlined />
-          Post New Blog
+          {blogProp ? "Update Blog" : "Post New Blog"}
         </Space>
       }
       onCancel={onCancel}
@@ -54,12 +83,12 @@ export default function ModalBlogActions({ isModalOpen, onOK, onCancel }) {
         <Button key="submit" type="primary" onClick={handleSubmitForm}>
           <Space size="small">
             <DiffOutlined />
-            Post New Blog
+            {blogProp ? "Update Blog" : "Post New Blog"}
           </Space>
         </Button>,
       ]}
     >
-      <Form form={form} layout="vertical">
+      <Form form={form} layout="vertical" initialValues={initialValues}>
         <Form.Item
           label="Title Post"
           name="title"
